38e762a6de83c59c43737df02cc84c49441b4814
chaifangchen <chaifangchen@kaochong.com>
2018-07-06 17:21:14 +0800

    【fangchen】添加前端代码

--- src/main/java/com/cfc/admin/controller/AdminNewApiController.java
+++ src/main/java/com/cfc/admin/controller/AdminNewApiController.java
@@ -2,6 +2,7 @@ package com.cfc.admin.controller;
 
 import com.cfc.admin.service.AdminNewService;
 import com.cfc.util.KCUtil;
+import com.cfc.util.TextUtils;
 import com.cfc.util.controller.BaseApiController;
 import com.cfc.util.model.PageModel;
 import com.cfc.util.spring.SpringMVCUtil;
@@ -32,7 +33,9 @@ public class AdminNewApiController extends BaseApiController {
     public Object add(){
         Map<String, Object> params = KCUtil.getParameterMap(SpringMVCUtil.getRequest());
         Map<String, Object> data = new HashMap<>();
-        data.put("id", params.get("id"));
+        if (!TextUtils.isEmpty(params.get("id") + "")) {
+            data.put("id", params.get("id"));
+        }
         data.put("title", params.get("title"));
         data.put("index", params.get("index"));
         data.put("content", params.get("editorValue"));
@@ -40,7 +43,7 @@ public class AdminNewApiController extends BaseApiController {
         data.put("type", params.get("type"));
         data.put("ctime", new Date());
         data.put("utime", new Date());
-        return genReturnJSON(AdminNewService.createNew(data, Integer.valueOf(params.get("id")+"")));
+        return genReturnJSON(AdminNewService.createNew(data, Integer.valueOf(data.get("id") + "")));
 
     }
 
