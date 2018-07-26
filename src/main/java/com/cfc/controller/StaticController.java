cdba2f568458dcfdcba8f0dfee5215315683e386
chaifangchen <chaifangchen@kaochong.com>
2018-07-26 17:49:33 +0800

    【fangchen】增加robotscontroller

---
+++ src/main/java/com/cfc/controller/StaticController.java
@@ -0,0 +1,19 @@
+package com.cfc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * author fangchen
+ * date  2018/7/26 下午5:45
+ */
+@Controller
+public class StaticController {
+
+    @RequestMapping("/robots.txt")
+    public String getRobots(){
+
+        return "redirect:/static/robots.txt";
+    }
+}
